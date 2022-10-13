9283b68ccef81986202f748f29819a0631816db3
freitzzz <1160907@isep.ipp.pt>
2022-10-13 20:47:49 +0100

    simplify index

--- index.js
+++ index.js
@@ -1,56 +1,12 @@
 import express from 'express';
-import cors from 'cors';
 
-import { Header, Status, Url } from './src/relay.js';
-import axios from 'axios';
-
-axios.defaults.validateStatus = (_) => true;
+import { Body, Cors, Handler } from './src/relay.js';
 
 const app = express();
+const cors = Cors.default();
 
-const corsOptions = {
-    credentials: true
-};
-
-const bodyLimit = { limit: Number.MAX_SAFE_INTEGER };
-
-app.use(express.raw(bodyLimit));
-app.use(express.text(bodyLimit));
-app.use(express.json(bodyLimit));
-app.use(express.urlencoded(bodyLimit));
-
-app.all('*', cors(corsOptions), handler);
-
-function handler(req, res) {
-    const headers = req.headers;
-
-    const destinationUrl = headers[Header.destinationUrlHeader];
-
-    if (destinationUrl === undefined) {
-        return bozoReply(res);
-    } else {
-        return relay(req, res);
-    }
-}
-
-async function relay(req, res) {
-    const destinationRequest = Header.expressToAxiosRequest(req);
-
-    try {
-        const response = await axios.request(destinationRequest);
-        res.headers = response.headers;
-
-        return res.status(response.status).send(response.data);
-    } catch (error) {
-        console.error(error);
-
-        return res.status(Status.unknownErrorStatusCode).send(Status.unknownErrorReplyData);
-    }
-}
-
-function bozoReply(res) {
-    return res.redirect(Url.bozoCorsRedirectUrl);
-}
+Body.parsers.forEach((p) => app.use(p));
+app.all('*', cors, Handler.default);
 
 const port = process.env.PORT;
 app.listen(port !== undefined ? port : 8080);
