59c5671224c601d83d9a92b4b821b76b3de3c8bb
freitzzz <1160907@isep.ipp.pt>
2022-10-13 20:47:20 +0100

    export new files in barrel file

--- src/relay.js
+++ src/relay.js
@@ -1,3 +1,7 @@
+export * as Axios from './axios.js';
+export * as Body from './body.js';
+export * as Cors from './cors.js';
+export * as Handler from './handler.js';
 export * as Header from './header.js';
 export * as Status from './status.js';
 export * as Url from './url.js';
