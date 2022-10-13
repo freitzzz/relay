13e9b1ef25666c9cd05c9e6a0b5dbebcf1460861
freitzzz <1160907@isep.ipp.pt>
2022-10-13 20:47:07 +0100

    move express handler logic to handler.js

---
+++ src/handler.js
@@ -0,0 +1,35 @@
+import { Axios, Header, Status, Url } from "./relay.js";
+
+const axios = Axios.default;
+
+async function relay(req, res) {
+    const destinationRequest = Header.expressToAxiosRequest(req);
+
+    try {
+        const response = await axios.request(destinationRequest);
+        res.headers = response.headers;
+
+        return res.status(response.status).send(response.data);
+    } catch (error) {
+        console.error(error);
+
+        return res.status(Status.unknownErrorStatusCode).send(Status.unknownErrorReplyData);
+    }
+}
+
+
+function bozo(res) {
+    return res.redirect(Url.bozoCorsRedirectUrl);
+}
+
+export default function (req, res) {
+    const headers = req.headers;
+
+    const destinationUrl = headers[Header.destinationUrlHeader];
+
+    if (destinationUrl === undefined) {
+        return bozo(res);
+    } else {
+        return relay(req, res);
+    }
+}
